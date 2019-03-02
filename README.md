d112f09bce4a7409a6af0ba7f87485dea48ecad5
frthjf <lifeworld-api@posteo.net>
2019-03-02 17:15:22 +0000

    Big bang

---
+++ README.md
@@ -0,0 +1,13 @@
+# Outbox
+
+A simple solution to send out emails from the OctoberCMS backend
+
+**Features**
+
+- Quickly compose emails and send emails from the backend
+
+Useful to test the email configuration and send branded emails.
+
+---
+
+Navigate to Outbox and compose an email. The mail uses the ``nocio.outbox::mail.template`` mail template.
---
+++ lang/en/lang.php
@@ -0,0 +1,14 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Outbox',
+        'description' => 'Use mail templates to send branded emails',
+    ],
+    'composer' => [
+        'send' => 'Send',
+        'sent_successfully' => 'Email successfully sent'
+    ],
+    'outbox' => [
+        'access' => 'Access outbox',
+        'tab' => 'Outbox',
+    ],
+];
